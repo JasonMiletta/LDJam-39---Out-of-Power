abf9e85f3f0e7eaa318c18b9b0d768a63707aa2f
Jason Miletta <jmiletta01@gmail.com>
2017-07-29 16:15:44 -0500

    Add Mouse Look Tracking to player character

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,18 +6,35 @@ public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed = 1.0f;
 
+    private bool isMovementLocked = false;
+    
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        look();
+        if (isMovementLocked)
+        {
+            movement();
+        }
+    }
+
+    private void look()
+    {
+        var ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray, Color.yellow);
+        
+        transform.LookAt(ray, new Vector3(0, 0, -1));
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+
+    private void movement()
+    {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
 
-        transform.Translate(x, 0, z);
+        transform.Translate(x, 0, z, Space.World);
     }
-
-
 }
