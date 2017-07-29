cb3087da1dae9d15cf9d49877c939a4e602af48a
Jason Miletta <jmiletta01@gmail.com>
2017-07-29 15:55:34 -0500

    Add camera tracking

---
+++ Assets/Camera_PlayerTracking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_PlayerTracking : MonoBehaviour {
+    public float smoothTime = 0.3f;
+
+    private GameObject player;
+    private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = player.transform.position;
+            targetPosition.y = this.transform.position.y;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
