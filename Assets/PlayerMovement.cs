eba3d3b97f331aa416f0d3649734e794f72a25b7
Jason Miletta <jmiletta01@gmail.com>
2017-07-29 15:48:46 -0500

    Add rudimentary player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public float movementSpeed = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,11 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Translate(x, 0, z);
+    }
+
+
 }
