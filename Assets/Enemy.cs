ea4c9a6e5c7c9c69761979b80d427aa91520a9c8
Jason Miletta <jmiletta01@gmail.com>
2017-07-29 22:42:21 -0500

    Add enemies with Chasing and Death functions. Add initial UI

---
+++ Assets/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float movementSpeed = 1.0f;
+
+    private GameObject target;
+    private bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isAlive)
+        {
+            moveToTarget();
+        }
+	}
+
+    private void moveToTarget()
+    {
+        var movementVector = target.transform.position - transform.position;
+        transform.Translate(movementVector * Time.deltaTime * movementSpeed, Space.World);
+    }
+
+    //Oh god this is gruesome huh?
+    private void killSelf(Vector3 deathForce)
+    {
+        isAlive = false;
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = false;
+        rigidbody.AddForce(deathForce * 20.0f, ForceMode.Impulse);
+        Destroy(this.gameObject, 5.0f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+        if (playerMovement != null)
+        {
+            if (playerMovement.isDashing)
+            {
+                //Player was dashing, kill this enemy
+                var deathVector = transform.position - collision.transform.position;
+                killSelf(deathVector);
+
+            } else
+            {
+                //Hurt player?
+            }
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed = 1.0f;
+    public float maxDashTime = 0.25f;
+    public float dashSpeed = 2.0f;
+    public float dashStoppingSpeed = 0.1f;
+    public bool isDashing = false;
 
     private bool isMovementLocked = false;
-    
+    private float currentDashTime;
+    private Vector3 stashedDashVector;
+
 	// Use this for initialization
 	void Start () {
+        currentDashTime = maxDashTime;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        look();
+	void Update ()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+
+        chargeLoop();
+        lookLoop();
         if (!isMovementLocked)
         {
-            movement();
+            movementLoop();
         }
     }
 
-    private void look()
+    private void lookLoop()
     {
         var ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Debug.DrawRay(Camera.main.transform.position, ray, Color.yellow);
@@ -30,11 +41,63 @@ public class PlayerMovement : MonoBehaviour {
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
     }
 
-    private void movement()
+    private void movementLoop()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
-
         transform.Translate(x, 0, z, Space.World);
     }
+
+    private void chargeLoop()
+    {
+        //Check if we're still dashing
+        if (currentDashTime < maxDashTime)
+        {
+            Debug.Log("Dashing");
+            isDashing = true;
+            currentDashTime += Time.deltaTime;
+            transform.Translate(stashedDashVector * Time.deltaTime * dashSpeed, Space.World);
+            isMovementLocked = true;
+        }
+        else
+        {
+            isDashing = false;
+            if(Input.GetButton("Fire1") != true)
+            {
+                isMovementLocked = false;
+            }
+            
+            if (Input.GetButtonDown("Fire1"))
+            {
+                isMovementLocked = true;
+                startChargeParticles();
+
+            } 
+            else if (Input.GetButtonUp("Fire1"))
+            {
+                stopChargeParticles();
+                currentDashTime = 0.0f;
+                stashedDashVector = transform.forward;
+            }
+        }
+    }
+
+    private void startChargeParticles()
+    {
+        var chargeParticle = GetComponentInChildren<ParticleSystem>();
+        if (chargeParticle != null)
+        {
+            chargeParticle.Play();
+        }
+    }
+
+    private void stopChargeParticles()
+    {
+        var chargeParticle = GetComponentInChildren<ParticleSystem>();
+        if (chargeParticle != null)
+        {
+            chargeParticle.Stop();
+        }
+    }
+
 }
