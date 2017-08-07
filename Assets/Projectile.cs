c409ac254a5e75fdcc65107abd88af059fd3ddc0
Jason Miletta <jmiletta01@gmail.com>
2017-08-06 22:41:34 -0500

    Implement projecticle hits

---
+++ Assets/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public GameObject particleBurst;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement player = other.gameObject.GetComponent<PlayerMovement>();
+        if(player != null)
+        {
+            Debug.Log("Hit!");
+            if(particleBurst != null)
+            {
+                var newParticleBurst = Instantiate(particleBurst, transform.position, transform.rotation);
+                Destroy(newParticleBurst, 5.0f);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
